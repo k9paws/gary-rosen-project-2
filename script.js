86f59795acd5f7de189743e5625a5286070d1101
Gary Rosen <candogservices@gmail.com>
2021-06-06 21:45:53 -0400

    Working MVP

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
 // 1. Look up API documentation for how to get the data I want "FETCH the data"
 const provincesSelectElement = document.getElementById("provinces");
 
+const resultsElement = document.getElementById("results");
+
 fetch("https://api.covid19tracker.ca/provinces")
   .then((response) => response.json())
   .then((listOfProvinces) => {
@@ -20,5 +22,16 @@ fetch("https://api.covid19tracker.ca/provinces")
   const formElement = document.getElementById("search-form");
   formElement.onsubmit = (event) => {
     event.preventDefault();
-    console.log(provincesSelectElement.value);
+    // console.log(provincesSelectElement.value);
+    fetch(`https://api.covid19tracker.ca/summary/split/`)
+      .then((response) => response.json())
+      .then((summaryData) => {
+        // console.log(summaryData.data);
+        const selectedProvData = summaryData.data.find(
+            item => item.province === provincesSelectElement.value
+        )
+        // console.log(selectedProvData.total_vaccinations);
+        resultsElement.innerText = selectedProvData.total_vaccinations;
+      });
   }
+
