578a9557c3130cf7592e0b5833d84469fa2f95e6
Gary Rosen <candogservices@gmail.com>
2021-06-06 21:05:52 -0400

    Rendered Provinces from API to Select Element

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 
 
 
-// 1. Look up API documentation for how to get the data I want "FETCH the data"
 
+
+// 1. Look up API documentation for how to get the data I want "FETCH the data"
 
 fetch("https://api.covid19tracker.ca/provinces")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((listOfProvinces) => {
+    const provincesSelectElement = document.getElementById("provinces");
+    listOfProvinces.forEach(province => {
+      const provOption = document.createElement("option");
+      provOption.value = province.code;
+      provOption.text = province.name;
+      provincesSelectElement.add(provOption);
+    });
+  });
