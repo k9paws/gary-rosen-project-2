b56e9a0a63b2a5f56c95eafce10fc969ea105b0d
Gary Rosen <candogservices@gmail.com>
2021-06-06 21:22:53 -0400

    Inital hanlding of form submit

--- script.js
+++ script.js
@@ -4,11 +4,11 @@
 
 
 // 1. Look up API documentation for how to get the data I want "FETCH the data"
+const provincesSelectElement = document.getElementById("provinces");
 
 fetch("https://api.covid19tracker.ca/provinces")
   .then((response) => response.json())
   .then((listOfProvinces) => {
-    const provincesSelectElement = document.getElementById("provinces");
     listOfProvinces.forEach(province => {
       const provOption = document.createElement("option");
       provOption.value = province.code;
@@ -16,3 +16,9 @@ fetch("https://api.covid19tracker.ca/provinces")
       provincesSelectElement.add(provOption);
     });
   });
+
+  const formElement = document.getElementById("search-form");
+  formElement.onsubmit = (event) => {
+    event.preventDefault();
+    console.log(provincesSelectElement.value);
+  }
