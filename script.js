e6d34fc3ba22ec4054642af9420d681df291de8e
Gary Rosen <candogservices@gmail.com>
2021-06-06 21:52:06 -0400

    Namedspaced code

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
+const covidVaccinationApp = {};
 
+covidVaccinationApp.provincesSelectElement =
+  document.getElementById("provinces");
 
+covidVaccinationApp.resultsElement = document.getElementById("results");
 
-
-
-// 1. Look up API documentation for how to get the data I want "FETCH the data"
-const provincesSelectElement = document.getElementById("provinces");
-
-const resultsElement = document.getElementById("results");
-
-fetch("https://api.covid19tracker.ca/provinces")
-  .then((response) => response.json())
-  .then((listOfProvinces) => {
-    listOfProvinces.forEach(province => {
-      const provOption = document.createElement("option");
-      provOption.value = province.code;
-      provOption.text = province.name;
-      provincesSelectElement.add(provOption);
+covidVaccinationApp.init = () => {
+  // 1. Look up API documentation for how to get the data I want "FETCH the data"
+  fetch("https://api.covid19tracker.ca/provinces")
+    .then((response) => response.json())
+    .then((listOfProvinces) => {
+      listOfProvinces.forEach((province) => {
+        const provOption = document.createElement("option");
+        provOption.value = province.code;
+        provOption.text = province.name;
+        covidVaccinationApp.provincesSelectElement.add(provOption);
+      });
     });
-  });
 
   const formElement = document.getElementById("search-form");
   formElement.onsubmit = (event) => {
     event.preventDefault();
-    // console.log(provincesSelectElement.value);
+    // console.log(covidVaccinationApp.provincesSelectElement.value);
     fetch(`https://api.covid19tracker.ca/summary/split/`)
       .then((response) => response.json())
       .then((summaryData) => {
         // console.log(summaryData.data);
         const selectedProvData = summaryData.data.find(
-            item => item.province === provincesSelectElement.value
-        )
+          (item) => item.province === covidVaccinationApp.provincesSelectElement.value
+        );
         // console.log(selectedProvData.total_vaccinations);
-        resultsElement.innerText = selectedProvData.total_vaccinations;
+        covidVaccinationApp.resultsElement.innerText =
+          selectedProvData.total_vaccinations;
       });
-  }
+  };
+};
 
+covidVaccinationApp.init();
